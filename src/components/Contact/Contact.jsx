fea03994df47e1d9bd2443d27449902f0a4841d7
Herman Rommetveit <herman.rommetveit@gmail.com>
2020-01-22 17:03:18 +0000

    Google maps

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { StyledOuter, StyledContainer } from "../../commons/StyledComponents";
 import SocialFollow from "../SocialFollow/SocialFollow";
+import Map from "../GoogleMaps/Map";
 
 const Contact = () => {
   return (
     <StyledOuter>
       <StyledContainer className="animated fadeIn">
         <h3 style={{color: "white"}}>Kontakt</h3>
+        <div style={{ height: "auto", width: "auto" }}>
+          <Map></Map> 
+        </div>
         <SocialFollow></SocialFollow>
       </StyledContainer>  
     </StyledOuter>
