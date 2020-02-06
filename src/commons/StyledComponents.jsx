fb45e3bbc5c5cb25a1a88379d20695b93a87a161
hermanro77 <herman.rommetveit@gmail.com>
2020-02-06 17:19:59 +0100

    favicon og tittel

--- src/commons/StyledComponents.jsx
+++ src/commons/StyledComponents.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledOuter = styled.main`
   height: calc(var(--vh) * 100);
-  
+  height: 100%;
   display: flex;
   background-image: url("/images/river.jpg");
   background-size: cover;
@@ -21,31 +21,18 @@ export const StyledOuter = styled.main`
 
 export const StyledContainer = styled.div`
   padding: 2rem;
-  min-height: calc(var(--vh) * 90 - 6rem);
-  max-height: calc(var(--vh) * 90 - 6rem);
-  
+  min-height: calc(var(--vh) * 95 - 4rem);
+  max-height: calc(var(--vh) * 95 - 4rem);
+  min-width: 95vw;
+  max-width: 95vw;
   text-align: center;
-
-  width: 100vh;
-  min-width: calc(95% - 12rem);
-  max-width: calc(95% - 12rem);
   background-color: rgba(20, 20, 20, 0.75);
   border-radius: 6px;
-  box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 15px 30px 0px rgba(0, 0, 0, 0.2);
   position: relative;
-  margin-left: 5rem;
-  margin-right: auto;
+  margin: auto;
+  top: calc(var(--vh) * 5);
   overflow: auto;
-  
-
-  @media screen and (max-width: 1400px) {
-    min-height: calc(var(--vh) * 94 - 4rem);
-    
-    min-width: calc(95%);
-    margin: auto;
-    margin-top: 5rem;
-    overflow: auto;
-  }
 
   @media screen and (max-width: 768px) {
     padding: 1.5rem;
